2ddce35b76581c3ed7147bc689d83b5b555d46b2
mohamed bella <90554606+mohamedBella2000@users.noreply.github.com>
2022-06-30 03:00:30 +0100

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,5 +1,5 @@
-let handler =  m => m.reply(` ❰𝐀𝐫𝐜𝐭𝐢𝐱 × 𝐀𝐫𝐜𝐞𝐮𝐬 𝐢𝐧𝐜.❱
-https://chat.whatsapp.com/Kb6msWfU8fP25KcG3M4Xtw
+let handler =  m => m.reply(` ❰بلا محمد ❱
+https://facebook.com/Bella.lewox
 `.trim()) // Add yourself if you want
 handler.help = ['donate']
 handler.tags = ['info']
